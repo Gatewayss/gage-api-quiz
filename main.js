81811c0865d1ae1cb473bab1f3f4640e48b8c404
Gage Norton <nortongage0@gmail.com>
2022-12-21 17:26:26 -0600

    Add todo list in comments

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ function startQuiz() {
     quiz.setAttribute('style', "visibility: visible;")
   }
 
+/* 485 break point needs to be modified */
+
+/* TODO: 
+create questions and answers,
+put those in a object, 
+make a next button,
+use text content to change the test after each question after clicking on the next content,
+have the answers set to true or false and display
+them if user gets them right and add to score,
+create the count down,
+if they get a question wrong subtract time 
+create the score that updates,
+
+create a finish page 
+create a high score page 
+figure out local storage to save score
+*/
