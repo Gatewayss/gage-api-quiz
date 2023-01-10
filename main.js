7c5a01e9876ca948318dda9bb5602514da60efda
Gage Norton <nortongage0@gmail.com>
2023-01-09 21:34:50 -0600

    Revert files back to previous commit

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ backBtn.addEventListener("click", () => {
 exitBtn.addEventListener("click", () => {
     currentScore = 0;
     score.textContent = currentScore;
-    $('#timer').removeClass('timer-red')
+    timer.style.color = '#fff';
     currentQuestionIndex = 0;
     counterBegun = false;
     $('#loser-container').toggleClass('hidden')
@@ -149,7 +149,7 @@ function quizTimer() {
         currentTime--;
         timer.textContent = currentTime 
         if (currentTime <= 10) {
-            $('#timer').addClass('timer-red')
+            $('#timer').css('color', 'red')
         }
         if (currentTime  <= 0) {
             clearInterval(timeLeft)
